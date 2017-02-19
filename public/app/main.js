49c821bb600b38747f845e17fcd0ea5bd4ca0abe
Jafar Salami <Jafar Salami>
2017-02-19 17:25:07 +0000

    the api

---
+++ public/app/main.js
@@ -0,0 +1,117 @@
+'use strict';
+
+(function() {
+
+class MainController {
+
+ constructor($http,$mdMedia,$mdDialog) {
+    this.message = 'Hello';
+    this.$http=$http;
+    this.appointment = [];
+    this.slots = [];
+    this.$mdMedia = $mdMedia;
+    this.$mdDialog = $mdDialog;
+    
+    // Configure dates
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; 
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd='0'+dd
+    } 
+
+    if(mm<10) {
+        mm='0'+mm
+    } 
+    // Create date and slots array
+    this.days = [{dd:dd,mm:mm,yyyy:yyyy},{dd:dd+1,mm:mm,yyyy:yyyy},{dd:dd+2,mm:mm,yyyy:yyyy},{dd:dd+3,mm:mm,yyyy:yyyy}];
+    this.slots= [{h:'10',m:'00'},{h:'10',m:'15'},{h:'10',m:'30'},{h:'10',m:'45'},{h:'11',m:'00'},{h:'11',m:'15'},{h:'11',m:'30'}];
+  }
+  
+  save(appointment){
+    appointment.active=true;
+    this.$http.post('/api/appointments',appointment).then(res=>{
+        this.dates = this.allot(this.slots,this.days,this.appointments);
+        this.$http.get('/api/appointments').then(response=>{
+            this.appointments=response.data;
+            this.dates = this.allot(this.slots,this.days,this.appointments);
+        });
+    });
+        
+  }
+  
+  $onInit(){
+    var vm = this;
+    this.$http.get('/api/appointments').then(response=>{
+        this.appointments=response.data;
+        this.dates = this.allot(this.slots,this.days,this.appointments);
+    });
+  }
+  delete(d){
+      this.$http.delete('/api/appointments/'+d._id);
+  }
+  
+
+allot(slots, days, appointments){
+ var a = [];
+    _.each(days, function(d) { 
+        var k=new Date(d.yyyy,d.mm,d.dd);
+        
+        var v = [];
+        _.each(slots, function(s) { 
+            var x = new Date(d.yyyy,d.mm,d.dd,s.h,s.m);
+            var mx = moment(x);
+            var active = true;
+            _.each(appointments, function(g) { 
+                var sdt = moment(new Date(g.date));
+                if(moment.duration(sdt.diff(mx))._milliseconds===0){
+                    active = false;
+                }
+            })
+            v.push({date:x,active:active});
+        })
+        a.push({k:k,v:v});
+    })
+return a;
+}
+
+
+  showAdvanced(slot) {
+      var vm = this;
+    var useFullScreen = (this.$mdMedia('sm') || this.$mdMedia('xs'))  && this.customFullscreen;
+    this.$mdDialog.show({
+      controller: function($scope,$mdDialog,slot){
+          $scope.customer = {};
+          $scope.customer.slot = slot;
+        $scope.answer = function(answer){
+            $mdDialog.hide(answer);
+        };
+      },
+      templateUrl: 'app/customer.html',
+      locals : {
+          slot : slot
+      },
+      clickOutsideToClose:true,
+      fullscreen: useFullScreen
+    })
+    .then(function(answer) {
+        answer.date = answer.slot.date;
+        vm.save(answer);
+    });
+    
+  }
+  
+  
+ 
+
+}
+
+angular.module('appointmentApp')
+  .component('main', {
+    templateUrl: 'app/main.html',
+    controller: MainController
+  });
+
+})();
---
+++ server/index.js
@@ -0,0 +1,26 @@
+
+// Dependencies
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+// MongoDB
+
+mongoose.connect('mongodb://salamij:Osalami20_@ds147979.mlab.com:47979/8080');
+// mongoose.connection.on('error', function(){});
+
+// Express
+var app = express();
+
+app.use(express.static(__dirname + './../public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api', require('./api/appointment/'));
+
+// Start server
+app.listen(3000);
+
+console.log('Running on port 3000');
